7986aa103f7061f31cdf089916fbc003e8c09e7c
TheGuardianWolf <lichking21st@windowslive.com>
2017-05-09 02:55:07 +1200

    Fixed TFF.

--- generic/TFF.vhdl
+++ generic/TFF.vhdl
@@ -5,16 +5,16 @@ entity TFF is
     port( 
         T: in std_logic;
         Q: out std_logic := '0';
-        NQ: out std_logic := '1';
+        NQ: out std_logic := '1'
     );
 end TFF;
 
 architecture behavior of TFF is
 begin
-    process(clk)
+    process(T)
         variable value: std_logic := '0';
     begin
-        if(rising_edge(clk)) then
+        if(rising_edge(T)) then
             NQ <= value;
             value := not value;
             Q <= value; 
