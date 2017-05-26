9b2f2c0e7836e98579be27123af8ed9a80793951
TheGuardianWolf <lichking21st@windowslive.com>
2017-05-26 19:04:13 +1200

    Seeder updated.

--- generic/seeder.vhdl
+++ generic/seeder.vhdl
@@ -8,12 +8,26 @@ use ieee.numeric_std.all;
 entity seeder is
     port(
         clk_50M: in std_logic;
-        lfsr_seed: out std_logic_vector(15 downto 0) := "1010010110101000"
+        lfsr_seed: out std_logic_vector(15 downto 0) := (others => '0')
     );
 end entity;
 
 architecture behavior of seeder is
-
+    component lfsr_g is
+        port (
+            Clk: in std_logic;
+            Reset: in std_logic;
+            Enable: in std_logic;
+            Seed: in std_logic_vector(15 downto 0);
+            Q: out std_logic_vector(15 downto 0) := (others => '0')
+        );
+    end component;
 begin
-
+    rand_seeder: lfsr_g port map(
+        clk_50M,
+        '0',
+        '1',
+        "1010010110101000",
+        lfsr_seed
+    );
 end architecture;
